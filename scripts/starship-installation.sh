e47000923ed51c65c511bbaab9bdcaeda6be239a
AlbertSolomon <solomonaalbert@gmail.com>
2023-04-29 09:29:46 +0200

    setting up starship 🚀 to persist on boot.

--- scripts/starship-installation.sh
+++ scripts/starship-installation.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+INIT_COMMAND='eval "$(starship init bash)"'
 
 if [ -n "$(which starship)" ]; then
     echo "Starship 🚀 is already installed !!!"
-
+    # echo 'eval "$(starship init bash)"' >> ~/.bashrc
 else
     echo "Installing Starship. 🚀"
 
     curl -sS https://starship.rs/install.sh | sh
     chmod +x install_starship.sh # run ./starship-installation.sh
+
+    echo "adding >> eval "$(starship init bash)" to ~/.bashrc file"
+    echo "$INIT_COMMAND"
+
 fi
 
