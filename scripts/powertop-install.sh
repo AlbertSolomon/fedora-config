97b47701caac35fd5843ba6dd669893e18cb7a37
AlbertSolomon <solomonaalbert@gmail.com>
2023-05-01 10:42:43 +0200

    automatically setting tunable on boot up in  powertop  ⏻

--- scripts/powertop-install.sh
+++ scripts/powertop-install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# make sure you read this guide [https://www.fosslinux.com/1599/powertop-for-fedora-helps-increase-laptops-battery-life.htm] before running this script
+
 if [ -n "$(which powertop)" ]; then
     echo "powertop is installed"
 else
     echo "powertop is not installed... Instlling powertop"
     sudo dnf install powertop
-    
+
+    echo "powertop installed successfully"
+
+    # automatically set all tunables to ‘Good,
+    echo "automatically setting all tunables to ‘Good at boot up (optimal power saving mode )"
+    sudo systemctl enable powertop.service
 fi
 
 chmod +x "$0" # $0 refers the script itself
