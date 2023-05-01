5fa1ce052857a2b005596d577068f38d27373a20
AlbertSolomon <solomonaalbert@gmail.com>
2023-05-01 10:12:51 +0200

    checking if powertop is installed ⏻

--- scripts/powertop-install.sh
+++ scripts/powertop-install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [-n "$(which powertop)"]; then
+if [ -n "$(which powertop)" ]; then
     echo "powertop is installed"
 else
     echo "powertop is not installed... Instlling powertop"
 fi
+
+chmod +x "$0"
