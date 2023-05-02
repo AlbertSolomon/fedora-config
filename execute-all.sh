b65769781a7d320bdcdde837c43d8549081caa20
AlbertSolomon <solomonaalbert@gmail.com>
2023-05-02 10:04:01 +0200

    we are executing them all 🪓

--- execute-all.sh
+++ execute-all.sh
@@ -4,5 +4,6 @@ echo "This will automatically run all scripts in the project"
 
 ./scripts/auto-cpufreq-installation.sh
 ./scripts/powertop-install.sh
+./scripts/starship/starship-installation.sh
 
 chmod +x "$0"
