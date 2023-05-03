e6cbb309ebf36094e8d7eeb5fb31355c307e8e6d
AlbertSolomon <solomonaalbert@gmail.com>
2023-05-03 11:17:46 +0200

    enabling snaps 🚀

--- scripts/auto-cpufreq-installation.sh
+++ scripts/auto-cpufreq-installation.sh
@@ -5,6 +5,11 @@ echo "1. Installing auto-cpufreq"
 # for installation we shall first install and enable snaps 
 echo "installing snaps... 🚀"
 
+if [ -n "$(which snap)" ]; then
+    echo "snaps are installed "
+else
+    sudo dnf install snapd
 
+fi
 
 chmod +x "$0"
