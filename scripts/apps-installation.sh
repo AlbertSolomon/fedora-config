cd17f7ccdd1a88aea5085d952c95ee1c63045573
AlbertSolomon <solomonaalbert@gmail.com>
2023-05-04 21:58:58 +0200

    ryt from the heart  🐺

--- scripts/apps-installation.sh
+++ scripts/apps-installation.sh
@@ -2,5 +2,11 @@
 
 echo "Installing all major apps..."
 
+# installing vs code 
 
+if [ -n "$(which code)" ]; then
+    echo "vs code is already installed"
+else
+    echo "INstalling vs code..."
+    
 chmod +x "$0"
