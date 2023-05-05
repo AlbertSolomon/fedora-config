fb04e43e3027ae35aed8f207b171155da34b7a01
AlbertSolomon <solomonaalbert@gmail.com>
2023-05-05 17:34:55 +0200

    we executing em all 💣

--- execute-all.sh
+++ execute-all.sh
@@ -4,6 +4,7 @@ echo "This will automatically run all scripts in the project"
 
 ./scripts/auto-cpufreq-installation.sh
 ./scripts/powertop-install.sh
+./scripts/asus-linux.sh
 ./scripts/starship/starship-installation.sh
 
 chmod +x "$0"
