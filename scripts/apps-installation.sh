9f26a685275247052df644daabbff11fdc139923
AlbertSolomon <solomonaalbert@gmail.com>
2023-05-06 22:04:23 +0200

    checking for updates repo 🗃

--- scripts/apps-installation.sh
+++ scripts/apps-installation.sh
@@ -11,5 +11,8 @@ else
 
     sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
     sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-    
+
+    echo "checking for updates..."
+    dnf check-update
+
 chmod +x "$0"
