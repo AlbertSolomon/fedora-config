6490374cfb2bf7eda2d9dda12ebdb33be8e38f42
AlbertSolomon <solomonaalbert@gmail.com>
2023-05-06 22:01:31 +0200

    adding vs code repo 🗃

--- scripts/apps-installation.sh
+++ scripts/apps-installation.sh
@@ -8,5 +8,8 @@ if [ -n "$(which code)" ]; then
     echo "vs code is already installed"
 else
     echo "INstalling vs code..."
+
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
     
 chmod +x "$0"
