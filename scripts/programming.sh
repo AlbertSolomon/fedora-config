cb85b8817c349a5bbd25f9c52d20873b25a78ab2
AlbertSolomon <solomonaalbert@gmail.com>
2023-05-14 19:44:18 +0200

    installing python on fedora... 😪

--- scripts/programming.sh
+++ scripts/programming.sh
@@ -2,5 +2,12 @@
 
 echo "Programming configuration...."
 
+# Installing python 
+if [ -n "$()" ]; then
+    echo "Python is already installed.. updating python"
+    
+else 
+    sudo dnf install python
+fi
 
 chmod +x "$0"
