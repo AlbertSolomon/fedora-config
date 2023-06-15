9ba2e70a68d17c75f336f33e2753e0176a6885d5
Albert Solomon <48782826+AlbertSolomon@users.noreply.github.com>
2023-06-15 16:30:37 -0400

    Update apps-installation.sh 🚤

--- scripts/apps-installation.sh
+++ scripts/apps-installation.sh
@@ -2,6 +2,11 @@
 
 echo "5. Installing all major apps..."
 
+# Speeding up DNF:
+
+echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
+echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
+
 # installing vs code 
 
 if [ -n "$(which code)" ]; then
