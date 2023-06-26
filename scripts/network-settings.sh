2352b4e7d144912a882aa6e0d4c8ee469e8a5aae
AlbertSolomon <solomonaalbert@gmail.com>
2023-06-26 20:57:15 +0200

    added network connection settings command...🖧

--- scripts/network-settings.sh
+++ scripts/network-settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo " Configuring network settings 🚀"
-
 sudo touch /etc/NetworkManager/conf.d/20-connectivity-fedora.conf
+echo "Configuration will be effective upon reboot..."
 
 chmod +x "$0"
