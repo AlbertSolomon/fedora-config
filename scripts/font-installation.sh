d518423ffba3da79a04ce60be9c8bf54eb0272c2
AlbertSolomon <solomonaalbert@gmail.com>
2023-07-23 15:49:24 +0200

    adding nerd fonts installation 😎

--- scripts/font-installation.sh
+++ scripts/font-installation.sh
@@ -8,4 +8,13 @@ flatpak install flathub org.gnome.FontManager
 echo "Installing font Dowloader 🚀"
 flatpak install flathub org.gustavoperedo.FontDownloader
 
+#lets check if a directory exists
+FONT_DIR = "$(~/.local/share/fonts/ -d)"
+if [ ! -d "$FONT_DIR" ]; then
+	mkdir -p ~/.local/share/fonts
+else
+	cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
+	# cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/JetBrainsMono-Regular.otf
+fi
+
 chmod +x "$0"
