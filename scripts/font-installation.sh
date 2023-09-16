cf82f5f2f47e1608bb07af612bcb4e816e300b73
AlbertSolomon <solomonaalbert@gmail.com>
2023-09-16 21:23:41 +0200

    advncing the font installation script, bugy though🦀

--- scripts/font-installation.sh
+++ scripts/font-installation.sh
@@ -9,12 +9,23 @@ echo "Installing font Dowloader 🚀"
 flatpak install flathub org.gustavoperedo.FontDownloader
 
 #lets check if a directory exists
-FONT_DIR = "$(~/.local/share/fonts/ -d)"
-if [ ! -d "$FONT_DIR" ]; then
-	mkdir -p ~/.local/share/fonts
-else
+FDIR= "~/.local/share/fonts/"
+if [ ! -d "${FDIR}" ]; then
+    mkdir -p "${FDIR}"
+fi
 	cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
 	# cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/JetBrainsMono/JetBrainsMono-Regular.otf
-fi
+
+	while read FONT;do
+		# cd $FONT
+		echo "downloading ${FONT} Nerd font"
+		wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/$FONT.tar.xz
+		echo "${FONT} Nerd font downloaded succefully..."
+		echo "Installing ${FONT} Nerd font...."
+		tar -xf $FONT.tar.xz && mv ./*.ttf ./ && rm -r README.md readme.md COPYING-LICENSE $FONT.tar.xz
+		echo "${FONT} Nerd font Installed..."
+	
+	done < fonts.txt
+#fi
 
 # chmod +x "$0"
