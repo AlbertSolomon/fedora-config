a4bc47aaaa071d7569daa14b3abcce4f1f2696bd
AlbertSolomon <solomonaalbert@gmail.com>
2023-11-09 23:03:38 +0200

    🦫🦫🦫🦫

--- execute-all.sh
+++ execute-all.sh
@@ -2,6 +2,8 @@
 
 echo "This will automatically run all scripts in the project"
 
+sudo dnf makecache --refresh
+
 ./scripts/auto-cpufreq-installation.sh
 ./scripts/powertop-install.sh
 ./scripts/asus-linux.sh
