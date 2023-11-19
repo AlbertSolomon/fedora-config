4383ab55fc79060881e67130154f6b5b75adc815
AlbertSolomon <solomonaalbert@gmail.com>
2023-11-19 22:05:34 +0200

    so we are trying to install docker compose,,,,,,,,,,,,,,,,,

--- scripts/programming.sh
+++ scripts/programming.sh
@@ -82,4 +82,12 @@ else
 	sudo systemctl enable docker
 fi
 
-chmod u+x "$0"
+if [ -n "$(which docker-compose)"]; then 
+	echo "docker-compose is already installed..."
+else
+	sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
+	sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+fi
+
+chmod u+x "$0"`````~~~~~~~~~~~~~``
