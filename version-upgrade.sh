2cc61ca87844dba1926e19942c6ae406581aba75
AlbertSolomon <solomonaalbert@gmail.com>
2024-07-23 19:22:03 +0200

    commited from NOCMA DP LL

--- version-upgrade.sh
+++ version-upgrade.sh
@@ -2,6 +2,6 @@
 echo "Updating packages, and refleshing the os"
 sudo dnf upgrade --refresh
 sudo dnf install dnf-plugin-system-upgrade
-sudo dnf system-upgrade download --releasever=39
+sudo dnf system-upgrade download --releasever=40
 sudo dnf system-upgrade reboot
 # refer to : https://docs.fedoraproject.org/en-US/quick-docs/upgrading-fedora-offline/
